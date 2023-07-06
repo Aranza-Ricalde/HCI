0dcf171a982eb897e24a7028de3bbbd51a757f28
César González <chesarglez429@gmail.com>
2023-07-05 19:13:38 -0600

    Updates

---
+++ precache-manifest.080545c72bc80d807dd29a3c1bd6b086.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/HCI/favicon.ico"
+  },
+  {
+    "revision": "26dadddc3cb2f2f76ed60ebc4a73a515",
+    "url": "/HCI/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/HCI/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/HCI/manifest.json"
+  },
+  {
+    "revision": "64b5e7db49007a98eea9",
+    "url": "/HCI/static/css/main.0bad0902.chunk.css"
+  },
+  {
+    "revision": "64b5e7db49007a98eea9",
+    "url": "/HCI/static/js/main.64b5e7db.chunk.js"
+  },
+  {
+    "revision": "707ca0bdf1770dd0270c",
+    "url": "/HCI/static/js/runtime~main.707ca0bd.js"
+  },
+  {
+    "revision": "b17b319b1ad2b352f0ab",
+    "url": "/HCI/static/js/vendors~main.b17b319b.chunk.js"
+  }
+]);
